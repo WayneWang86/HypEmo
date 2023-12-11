50029edd02e19b7d1b029ae9b2f04329c63cb115
EC2 Default User <ec2-user@ip-172-31-66-131.ec2.internal>
2023-12-11 01:47:53 +0000

    word2_aug completed
    Signed-off-by: EC2 Default User <ec2-user@ip-172-31-66-131.ec2.internal>

--- train.sh
+++ train.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 # Define an array of arguments
-args=("back_trans", "emo_to_text", "baseline", "gpt_aug", "multi-label", "noise_aug", "emoji_augmentation")
+args=("back_trans" "emo_to_text" "baseline" "gpt_aug" "multi-label" "noise_aug" "emoji_augmentation")
+
 
 # Loop through the array and execute the Python script with each argument
 for arg in "${args[@]}"
 do
-    python train.py "$arg"
+    echo "Start training for $arg"
+    export METHOD=$arg
+    python train.py
+    # echo $METHOD
+    echo "completed!"
+    echo " "
+    echo " "
+    # echo "$arg"
 done
